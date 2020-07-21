21fdc27786878a1e47cfd37a3d99d9aa77cd37a3
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-07-21 12:58:58 -0700

    added methods implementations

--- Percolation/src/Percolation.java
+++ Percolation/src/Percolation.java
@@ -3,12 +3,17 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class Percolation {
     private int N;
-    private boolean[] sites;
+    private boolean[][] sites;
     private int openSites;
     private WeightedQuickUnionUF UF;
 
     public Percolation(int n){
         if(n <= 0) throw new IllegalArgumentException("N must be greater than 0");
+        this.N = n;
+        UF = new WeightedQuickUnionUF(n + 2);
+        // connect the virtual top site to the top row
+        for(int i = 1;i <= N;++i)
+            UF.union(0, i);
     }
 
     public void open(int row, int col){
@@ -17,7 +22,7 @@ public class Percolation {
 
     public boolean isOpen(int row, int col){
         if(row <= 0 || row > N || col <= 0 || col > N) throw new IndexOutOfBoundsException("Row or Col index out of bounds");
-        throw new NotImplementedException();
+        return sites[row - 1][col - 1];
     }
 
     public boolean isFull(int row, int col){
@@ -30,7 +35,7 @@ public class Percolation {
     }
 
     public boolean percolates(){
-        throw new NotImplementedException();
+        return UF.connected(0, N * N + 1);
     }
 
     public static void main(String[] args) {
