59dc6465bd1cea4e66b7dca1780a414a635c804f
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-07-22 14:09:46 -0700

    added POINTSET implementation

--- KD_TREE/src/PointSET.java
+++ KD_TREE/src/PointSET.java
@@ -1,26 +1,31 @@
 import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.Queue;
 import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.SET;
 
 public class PointSET {
-    public PointSET(){
 
+    private SET<Point2D> redBlackTree;
+    public PointSET(){
+        redBlackTree = new SET<>();
     }
 
     public boolean isEmpty(){
-        return false;
+        return redBlackTree.isEmpty();
     }
 
     public int size(){
-        return 0;
+        return redBlackTree.size();
     }
 
     public void insert(Point2D p){
         if(p == null) throw new IllegalArgumentException();
+        redBlackTree.add(p);
     }
 
     public boolean contains(Point2D p){
         if(p == null) throw new IllegalArgumentException();
-        return false;
+        return redBlackTree.contains(p);
     }
 
     public void draw(){
@@ -29,12 +34,26 @@ public class PointSET {
 
     public Iterable<Point2D> range(RectHV rect){
         if(rect == null) throw new IllegalArgumentException();
-        return null;
+        Queue<Point2D> on_queue = new Queue<>();
+        for(Point2D p : redBlackTree){
+            if(rect.contains(p)){
+                on_queue.enqueue(p);
+            }
+        }
+        return on_queue;
     }
 
     public Point2D nearest(Point2D p){
         if(p == null) throw new IllegalArgumentException();
-        return null;
+        Point2D result = null;
+        double minDistance = Double.POSITIVE_INFINITY;
+        for(Point2D point : redBlackTree){
+            if(minDistance > p. distanceTo(point)){
+                result = point;
+                minDistance = p.distanceTo(point);
+            }
+        }
+        return result;
     }
 
     public static void main(String[] args) {
