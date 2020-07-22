3ed7ee632b7dfa671509afa907a2a073cebb28a4
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-07-22 14:45:06 -0700

    prototype implementation of kdtree

--- KD_TREE/src/KdTree.java
+++ KD_TREE/src/KdTree.java
@@ -18,6 +18,30 @@ public class KdTree {
 
     public void insert(Point2D p){
         if(p == null) throw new IllegalArgumentException();
+        root = insert(root, p, 0, 0, 1, 1, true);
+    }
+
+    public Node insert(Node x,Point2D p, double xmin, double ymin, double xmax, double ymax,boolean isVerticalOrientation){
+        if(x == null){
+            size++;
+            return new Node(p, new RectHV(xmin, ymin, xmax, ymax));
+        }
+        if(isVerticalOrientation){
+            int cmp = Double.compare(x.p.x(), p.x());
+            if(cmp < 0){
+                x.lb = insert(x.lb, p, xmin, ymin,x.p.x(), ymax, !isVerticalOrientation);
+            } else if(cmp > 0){
+                x.rt = insert(x.rt, p, x.p.x(), ymin, xmax, ymax, !isVerticalOrientation);
+            }
+        } else {
+            int cmp = Double.compare(x.p.y(), p.y());
+            if(cmp < 0){
+                x.lb = insert(x.lb, p, xmin, ymin, xmax, x.p.y(), !isVerticalOrientation);
+            } else if(cmp > 0){
+                x.rt = insert(x.rt, p, xmin, x.p.y(), xmax, ymax, !isVerticalOrientation);
+            }
+        }
+        return x;
     }
 
     public boolean contains(Point2D p){
@@ -26,7 +50,6 @@ public class KdTree {
     }
 
     public void draw(){
-
     }
 
     public Iterable<Point2D> range(RectHV rect){
@@ -48,5 +71,10 @@ public class KdTree {
         private RectHV rect;
         private Node lb;
         private Node rt;
+
+        public Node(Point2D p, RectHV rect){
+            this.p = p;
+            this.rect = rect;
+        }
     }
 }
