1fb4c727681c381e4b2e327a64d05dfaa75485df
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-07-22 13:48:45 -0700

    added KdTree api

---
+++ KD_TREE/src/KdTree.java
@@ -0,0 +1,43 @@
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+
+public class KdTree {
+    public KdTree(){
+
+    }
+
+    public boolean isEmpty(){
+        return false;
+    }
+
+    public int size(){
+        return 0;
+    }
+
+    public void insert(Point2D p){
+        if(p == null) throw new IllegalArgumentException();
+    }
+
+    public boolean contains(Point2D p){
+        if(p == null) throw new IllegalArgumentException();
+        return false;
+    }
+
+    public void draw(){
+
+    }
+
+    public Iterable<Point2D> range(RectHV rect){
+        if(rect == null) throw new IllegalArgumentException();
+        return null;
+    }
+
+    public Point2D nearest(Point2D p){
+        if(p == null) throw new IllegalArgumentException();
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
