17bc1196dca5b6c4fb053e4a20f48bd353d31ca2
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-07-23 12:06:02 -0700

    added draw method to Point

--- CollinearPoints/src/Point.java
+++ CollinearPoints/src/Point.java
@@ -1,24 +1,27 @@
+import edu.princeton.cs.algs4.StdDraw;
+
 import java.util.Comparator;
 
 public class Point implements Comparable<Point> {
 
-    private int x;
-    private int y;
+    private final int x;
+    private final int y;
 
     public Point(int x, int y){
-
+        this.x = x;
+        this.y = y;
     }
 
     public void draw(){
-
+        StdDraw.point(x, y);
     }
 
     public void drawTo(Point that){
-
+        StdDraw.line(this.x, this.y, that.x, that.y);
     }
 
     public String toString(){
-        return "";
+        return "(" + x + ", " + y + ")";
     }
 
     @Override
