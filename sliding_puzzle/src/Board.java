99bfcaf599b8eb0985d4bb60dc9bd4f577f33bb2
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-07-23 19:05:02 -0700

    added Board api

---
+++ sliding_puzzle/src/Board.java
@@ -0,0 +1,40 @@
+public class Board {
+    public Board(int[][] tiles){
+
+    }
+    public String toString(){
+
+    }
+
+    public int dimensions(){
+
+    }
+
+    public int hamming(){
+
+    }
+
+    public int manhattan(){
+
+    }
+
+    public boolean isGoal(){
+
+    }
+
+    public boolean equal(Object y){
+
+    }
+
+    public Iterable<Board> neighbors(){
+
+    }
+
+    public Board twin(){
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
