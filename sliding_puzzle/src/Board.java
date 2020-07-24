7ab19c2155b360fb48b870856e565d96e86ac55d
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-07-23 19:14:46 -0700

    added method implementations

--- sliding_puzzle/src/Board.java
+++ sliding_puzzle/src/Board.java
@@ -31,14 +31,27 @@ public class Board {
     }
 
     public boolean equal(Object y){
+        if(y == this){
+            return true;
+        }
+
+        if(y == null){
+            return false;
+        }
+
+        if(y.getClass() != this.getClass()){
+            return false;
+        }
+        Board b = (Board) y;
+        return Arrays.deepEquals(board, b.board);
     }
 
     public Iterable<Board> neighbors(){
-
+        return null;
     }
 
     public Board twin(){
-
+        return null;
     }
 
     public static void main(String[] args) {
