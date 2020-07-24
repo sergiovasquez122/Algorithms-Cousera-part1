be0959a417d9beb1083f90cf45c11dde11dc0924
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-07-24 12:35:41 -0700

    added exceptions

--- sliding_puzzle/src/Solver.java
+++ sliding_puzzle/src/Solver.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
+import edu.princeton.cs.algs4.Stack;
 
 public class Solver {
+
+    private boolean solvable;
+    private Stack<Board> moves;
+    public Solver(Board inital){
+        if(inital == null){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public boolean isSolvable(){
+        return solvable;
+    }
+
+    public int moves(){
+        if(!isSolvable()){
+            return -1;
+        }
+        return moves.size();
+    }
+
+    public Iterable<Board> solution(){
+        if(!isSolvable()){
+            return null;
+        }
+        return moves;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
